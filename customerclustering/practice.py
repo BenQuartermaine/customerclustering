b3222e8fad56e210e1177120fd9499b6e3fc3419
Ran Huo <huoran2805263@outlook.com>
2022-08-12 22:18:21 +1000

    included 'documented' as 'completed' in the queue.py and integrated db_connection to the file'

--- customerclustering/practice.py
+++ customerclustering/practice.py
@@ -3,7 +3,7 @@ import pymysql
 import pandas as pd
 from dotenv import load_dotenv, find_dotenv
 import datetime as dt
-from db_connection import Db
+from customerclustering.db_connection import Db
 
 class Practice:
     def __init__(self, conn):
@@ -70,7 +70,19 @@ class Practice:
         return merged_df
 
 if __name__ == '__main__':
-    conn = Db.db_conn()
+    #conn = Db.db_conn()
+
+    env_path = find_dotenv()
+    load_dotenv(env_path)
+
+    conn = pymysql.connect(
+    host=os.getenv('HOST'),
+    port=int(3306),
+    user=os.getenv('USER_DB'),
+    passwd=os.getenv('PASSWORD'),
+    db=os.getenv('DB'),
+    charset='utf8mb4')
+
 
     Prac=Practice(conn)
     df=Prac.get_practice_features()
