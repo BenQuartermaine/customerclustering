c4696e41bc48604e1ec2d230f78af1299ce16060
Ran Huo <huoran2805263@outlook.com>
2022-08-14 16:24:18 +1000

    import issue resolved! lol

--- customerclustering/documentation.py
+++ customerclustering/documentation.py
@@ -1,10 +1,10 @@
 import pandas as pd
-from db_connection import Db
+from customerclustering.db_connection import Db
 
 class Documenting:
     def __init__(self, conn):
         self.conn = conn
-        
+
 
     # Ratio of users subscribed to account age
     def get_ratio_subs_per_user(self):
@@ -14,29 +14,29 @@ class Documenting:
         """
         df = pd.read_sql_query(
             """
-                SELECT 
-                    Product, 
-                    Status, 
+                SELECT
+                    Product,
+                    Status,
                     userID,
                     stripeCustID,
                     COUNT(*) AS num_subs,
                     DATEDIFF(NOW(), createDate) AS account_age
                 FROM stripe_subscription
                 JOIN user ON user.stripeCustID = stripe_subscription.`Customer ID`
-                GROUP BY `Customer ID` 
+                GROUP BY `Customer ID`
                 ORDER BY COUNT(*) DESC
             """, self.conn)
 
         return df
-    
+
     def hello(activity_df):
         return ('hello', activity_df)
-    
+
     # Returns a df with averaeg minutes documented per resource, total mins and total documentations
     def get_total_mins_doc(self, activity_df):
         """
         Returns a DataFrame with:
-        total minutes documented, total documentations and the ratio of the two. 
+        total minutes documented, total documentations and the ratio of the two.
         Takes a while to run.
         """
         df_doc = pd.read_sql_query(
@@ -48,11 +48,11 @@ class Documenting:
             FROM activity_20220808
             GROUP BY owner
             """, self.conn)
-        
+
         df_doc['mins_per_documentation'] = round(df_doc['total_mins'] / df_doc['total_docs'], 0)
-        
+
         return df_doc
-    
+
 if __name__ == '__main__':
     conn = Db.db_conn()
     documenting = Documenting(conn)
