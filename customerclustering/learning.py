218611531847ec79284ec9ac4091da713d8cea48
Ran Huo <huoran2805263@outlook.com>
2022-08-19 22:36:41 +1000

    finished doc_per_year and resolved minPerYear issue

--- customerclustering/learning.py
+++ customerclustering/learning.py
@@ -31,11 +31,11 @@ class Learning:
 
         self.conn=conn
         self.df_act = df_act1
-        self.df_usr=pd.read_sql_query("SELECT * FROM user;", conn).drop_duplicates()
+        #self.df_usr=pd.read_sql_query("SELECT * FROM user;", conn).drop_duplicates()
 
     def get_activity_data(self):
         """
-        Returns a data frame with 'userID'
+        Returns a data frame with 'userID','activityType', 'providerName'
         """
         # clean data here
         # create a data frame with 'userID', 'activityType', 'providerName',
@@ -43,51 +43,98 @@ class Learning:
         df_act['providerName'].unique()
         #df_act['status'].unique()
 
+        #sort by 'userID' the 'min'
+        df_act=df_act.sort_values(by=['owner','min'],ascending=[True,True])
         # drop duplicated events
+
         df_act=df_act.drop_duplicates(subset=['activityID','owner','resource','min'],keep='last')
 
         # create minPerYear
         ## step1: get total min
-        selected_columns=['owner','providerName','min','updateDate','completeDate','activityType']
+        selected_columns=['activityID','owner','providerName','min','createDate','activityType']
         df_act=df_act[selected_columns]
-        #replace missing values of 'updateDate'
-        df_act['completeDate']=df_act['completeDate'].fillna(df_act['updateDate'])
 
-        #convert 'completeDate' to DateTime just to be safe
-        df_act['completeDate']=pd.to_datetime(df_act['completeDate'])
-
-        # replace 'N/A' by None
-        df_act['providerName'].replace('N/A',None,inplace=True)
-        # see if can get missing values from tracking event!!!!!!!!!!!!!!!!!!!! DO THIS!!!!!!!!!!!!!!!!!!!!!!!!!!
+        # get Years of the activities
+        df_act['createDate']=pd.to_datetime(df_act['createDate'])
+        df_act['createDate']=pd.DatetimeIndex(df_act['createDate']).year
 
 
 
 
-
+        df_act['providerName'].replace('N/A','Unknown',inplace=True)
+        df_act['providerName'].replace('','Unknown',inplace=True)
+        # see if can get missing values from tracking event!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! DO THIS!!!!!!!!!!!!!!!!!!!!!!!!!!
 
         # fill in missing value with 'Unknown'
         df_act['providerName']=df_act['providerName'].fillna('Unknown')
 
 
 
+
         # rename 'owner' as 'userID'
         df_act.rename(columns={'owner':'userID'},inplace=True)
+        #df_act.head(20)
         return df_act
 
+    def get_activity_per_year(self):
+        """
+        returns a dataframe with 'userID','createYear','numOfDoc','numOnAusmed','min','minOnAusmed'.
+        this dataframe is for reference only, with yearly num and min information.
+        """
+        # create a column "isAusmed": returns 1 if the provider is Ausmend
+        df_act1=self.get_activity_data()
+        df_act1['isAusmed']=df_act1['providerName'].apply(lambda x: 1 if ('Ausmed' in x) else 0 )
+        df_act1['minOnAusmed']=df_act1['min']*df_act1['isAusmed']
 
-    def get_Ausmed_year(self):
+        df_act1.head(20)
+
+        # create a column for the number of documetation
+        df_act1['numOfDoc']=1
+
+        df_act1=df_act1.groupby(['userID','createDate']).agg({'numOfDoc': sum,'isAusmed':sum, 'min': sum, 'minOnAusmed': sum}).reset_index()
+
+
+
+
+        # rename the columns
+        df_act1.rename(columns={ 'createDate': 'createYear','isAusmed':'numOnAusmed'},inplace=True)
+        df_act1.sort_values('userID',inplace=True)
+
+        return df_act1
+
+    def get_doc_per_year(self):
         """
-        Returns a data frame including 'userID','yearsOnAusmed'
+        returns a dataframe with 'userID','docPerYear','docOnAusmedPerYear',
+        'minPerYear','minOnAusmedPerYear',
+        'learnFromAusmedRatio_num','learnFromAusmedRatio_min'
         """
-        # get yearsOnAusmed
-        df_act=self.get_activity_data()
-        df_usr=self.df_usr[['userID','createDate']]
-        df_usr=df_usr.merge(df_act, on='userID', how='inner')
-        df_usr['createDate']=pd.to_datetime(df_usr['createDate'])
-        df_usr['completeDate']=pd.to_datetime(df_usr['completeDate'])
-        df_usr['yearsOnAusmed']=df_usr['completeDate']-df_usr['createDate']
-        df_usr=df_usr.groupby('userID').max().reset_index()
-        return df_usr
+
+        # get the docPerYear and minPerYear, learnFromAusmedRatio_min, learnFromAusmedRatio_num
+        df_act2=self.get_activity_per_year()
+        df_act2=df_act2.groupby('userID').mean().reset_index()
+        # rename the data frame
+        df_act2.rename(columns={'numOfDoc':'docPerYear','numOnAusmed':'docOnAusmedPerYear','min':'minPerYear','minOnAusmed':'minOnAusmedPerYear'},inplace=True)
+
+        # drop 'createYear'
+        df_act2.drop(columns=['createYear'],inplace=True)
+
+        # get ratios
+        df_act2['learnFromAusmedRatio_num']=df_act2['docOnAusmedPerYear']/df_act2['docPerYear']
+        df_act2['learnFromAusmedRatio_min']=df_act2['minOnAusmedPerYear']/df_act2['minPerYear']
+        return df_act2
+
+    def get_fav_activity(self):
+        """
+        returns a dataframe with 'userID','favActivityType' and 'secondFavActivityType'
+        """
+        df_act3=self.get_activity_data()
+
+        # create a replica of the activityType to get the secondFavActivityType
+        df_act3['secondFavActivityType']=df_act3['activityType']
+        df_act3=df_act3.groupby(['userID']).agg({'activityType': fav_fav, 'secondFavActivityType': fav_activityType}).reset_index()
+        df_act3
+
+        return df_act3
 
 
 
@@ -98,48 +145,15 @@ class Learning:
         Returns a data frame with 'userID','favoriteActivityType',
         '2ndfavoriteActivityType','minPerYear','min','percentageLearningFromAusmed'
         """
-        df_act=self.get_activity_data()
-        # create a column "isAusmed": returns 1 if the provider is Ausmend
-        df_act['isAusmed']=df_act['providerName'].apply(lambda x: 1 if ('Ausmed' in x) else 0 )
-        df_act['minOnAusmed']=df_act['min']*df_act['isAusmed']
-
-
-        # Get user's favorite and 2nd favorite activityType
-        # first create a copy of acticityType to get 2nd favorite activityType
-        df_act['2ndFavActivityType']=df_act['activityType']
-        df_act=df_act.groupby('userID').agg({'min': sum, 'minOnAusmed': sum, 'activityType': fav_fav, '2ndFavActivityType': fav_activityType}).reset_index()
-
-
-
-        # merge with df_usr
-        df_usr=self.get_Ausmed_year()
-        df_act=df_usr[['userID','yearsOnAusmed']].merge(df_act, on='userID', how='inner')
-
-
-        # rename the columns
-        df_act.rename(columns={'activityType': 'favActivityType'},inplace=True)
-        # some 'favActivityType' turn out to be a list, randomly select one
-
-        #df_act['favActivityType']=df_act['favActivityType'].apply(lambda x: clean_favActivityType(x))
-
-
-
-
-
-        # get Min learnt from Ausmed
-        df_act['yearsOnAusmed']=df_act['yearsOnAusmed']/ np.timedelta64(1, 'Y')
-        df_act['minOnAusmedPerYear']=df_act['minOnAusmed']/df_act['yearsOnAusmed']
-
-        # get percentageOfLearningFromAusmed
-        df_act['minPerYear']=df_act['min']/df_act['yearsOnAusmed']
-        df_act['percentageOfLearningFromAusmed']=df_act['minOnAusmed']/df_act['min']
-
-        # drop uneeded columns
-        #df_act=df_act.drop(columns=['minOnAusmed','minOnAusmedPerYear'])
-
+        df_act=self.get_doc_per_year().merge(
+            self.get_fav_activity(),on='userID', how='inner'
+        )
         # drop na
         df_act=df_act.dropna()
         return df_act
+
+
+
 
 if __name__ == '__main__':
     conn = Db.db_conn()
