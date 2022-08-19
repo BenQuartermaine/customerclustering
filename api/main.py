163f5cf0aec8e7de92fc3ec95391a417b9fc7c68
Ben Quartermaine <ben.quartermaine3@gmail.com>
2022-08-20 09:59:54 +1000

    tweaks to the joblib

--- api/main.py
+++ api/main.py
@@ -1,10 +1,7 @@
-from typing import Union
-
 from fastapi import FastAPI
-from customerclustering.get_training_data import GetTrainingData
-from customerclustering.db_connection import Db
-
 from fastapi.middleware.cors import CORSMiddleware
+import joblib
+import pandas as pd
 
 app = FastAPI()
 
@@ -16,13 +13,59 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-conn = Db.db_conn()
+model = joblib.load("model.joblib")
+
+#create notebook
+#load joblib (sklearn pipeline once loaded, takes normal pipeline functions like predict)
+#give it 1 - X rows of training data
+#test if it works
+#incorporate into predict 
+
+
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/test_route")
-def func():
-    h = GetTrainingData(conn, 30).get_training_data()
-    return h 
+@app.get("/predict")
+def func(Product, 
+            Status, 
+            userID, 
+            stripeCustID, 
+            num_subs, 
+            account_age, 
+            pProfileID, 
+            typeOfPractice, 
+            located, 
+            specialities, 
+            population, 
+            focus, 
+            complex, 
+            autonomy, 
+            access, 
+            startDate, 
+            endDate, 
+            createDate, 
+            country, 
+            favActivityType, 
+            secondFavActivityType, 
+            minPerYear, 
+            percentageOfLearningFromAusmed, 
+            numQueued, 
+            numCompletedFromQueue, 
+            minQueued, 
+            minCompleted, 
+            RatioOfCompletion_num, 
+            RatioOfCompletion_min, 
+            event_cpd_day_diff, 
+            doc_in_activation, 
+            activated, 
+            plan_type, 
+            subscribe_days, 
+            GoalsPerYear, 
+            ratioOfAchivedGoals, 
+            metaGoalTitle):
+    dict(Product, Status, userID, stripeCustID, num_subs, account_age, pProfileID, typeOfPractice, located, specialities, population, focus, complex, autonomy, access, startDate, endDate, createDate, country, favActivityType, secondFavActivityType, minPerYear, percentageOfLearningFromAusmed, numQueued, numCompletedFromQueue, minQueued, minCompleted, RatioOfCompletion_num, RatioOfCompletion_min, event_cpd_day_diff, doc_in_activation, activated, plan_type, subscribe_days, GoalsPerYear, ratioOfAchivedGoals, metaGoalTitle)
+    # model.predict(pd.dataFrane(dict))
+    return 'hello'
