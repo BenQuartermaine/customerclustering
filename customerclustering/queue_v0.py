fb4aab4585d003299581a361ca6c4a8bb9bb7acf
Ran Huo <huoran2805263@outlook.com>
2022-08-24 15:23:02 +1000

    getting minCompletedFromQueuePerYear

--- customerclustering/queue_v0.py
+++ customerclustering/queue_v0.py
@@ -15,7 +15,7 @@ class Queue:
     def get_event_data(self):
         """
         return a dataframe with 'userID', 'resourceID',
-        'eventType',source','resourceType','action','eventYear'
+        'eventType',source','resourceType','action'
         """
         #drop the last two columns as they have >80% missing value
         df_trk=self.df_trk.iloc[1:,:-2]
@@ -27,14 +27,11 @@ class Queue:
         # Get queue status (returns 1 if is queued)
         df_trk['resourceType']=df_trk['eventType'].apply(lambda x: x.split('_')[0])
         df_trk['action']=df_trk['eventType'].apply(lambda x: x.split('_')[1])
-        df_trk['eventDate']=pd.to_datetime(df_trk['eventDate'])
-        df_trk['eventYear']=pd.DatetimeIndex(df_trk['eventDate']).year
-
 
         return df_trk
 
 
-        # return a dataframe with userID, numOfResourcesToQueuePerYear, numOfCompletionFromQueuePerYear, RatioOfCompletion_num, minOfResourcesToQueue, minOfCompletedFromQueuePerYear, RatioOfCompletion_min
+        # return a dataframe with userID, numOfResourcesToQueue, numOfCompletionFromQueue, RatioOfCompletion_num, minOfResourcesToQueue, minOfCompletedFromQueue, RatioOfCompletion_min
     def get_queue_features(self,specify_source=False):
         print('Getting queue features')
         """
@@ -67,43 +64,41 @@ class Queue:
         # add mins from the resourceID
         df_res_select=self.df_res[['resourceID','min']].drop_duplicates()
         df_trk=df_trk.merge(df_res_select,on='resourceID')
-        df_trk['minQueuedPerYear']=df_trk['isQueued']*df_trk['min']
+        df_trk['minQueued']=df_trk['isQueued']*df_trk['min']
         df_trk['minCompletedFromQueue']=df_trk['CompletedFromQueue']*df_trk['min']
 
 
         # if 'specified_source=True'
-        df_trk1=df_trk.groupby(['userID','eventYear','source']).sum().reset_index()
-        df_trk1.rename(columns={'isQueued':'numQueuedPerYear','CompletedFromQueue':'numCompletedFromQueue'},inplace=True)
+        df_trk1=df_trk.groupby(['userID','source']).sum().reset_index()
+        df_trk1.rename(columns={'isQueued':'numQueued','CompletedFromQueue':'numCompletedFromQueue'},inplace=True)
 
 
 
 
         # if 'specified_source=False'
         if specify_source==False :
-            df_trk2=df_trk1.groupby(['userID','eventYear']).sum().reset_index()
-
-            df_trk2=df_trk.groupby('userID').mean().reset_index()
-            df_trk2['numQueuedPerYear']=df_trk2['numQueuedPerYear'].replace(0,-1)
-            df_trk2['minQueuedPerYear']=df_trk2['minQueuedPerYear'].replace(0,-1)
-            df_trk2['RatioOfCompletion_num']=np.abs(df_trk2['numCompletedFromQueue']/df_trk2['numQueuedPerYear'])
-            df_trk2['RatioOfCompletion_min']=np.abs(df_trk2['minCompletedFromQueue']/df_trk2['minQueuedPerYear'])
+            df_trk2=df_trk1.groupby(['userID']).sum().reset_index()
+            df_trk2['numQueued']=df_trk2['numQueued'].replace(0,-1)
+            df_trk2['minQueued']=df_trk2['minQueued'].replace(0,-1)
+            df_trk2['RatioOfCompletion_num']=np.abs(df_trk2['numCompletedFromQueue']/df_trk2['numQueued'])
+            df_trk2['RatioOfCompletion_min']=np.abs(df_trk2['minCompletedFromQueue']/df_trk2['minQueued'])
             # drop 'min' and 'isCompleted' as unnecessary
             df_trk2=df_trk2.drop(['min','isCompleted'],axis=1)
             #replace -1 back to 0
-            df_trk2['numQueuedPerYear']=df_trk2['numQueuedPerYear'].replace(-1,0)
-            df_trk2['minQueuedPerYear']=df_trk2['minQueuedPerYear'].replace(-1,0)
+            df_trk2['numQueued']=df_trk2['numQueued'].replace(-1,0)
+            df_trk2['minQueued']=df_trk2['minQueued'].replace(-1,0)
             return df_trk2
         #------------------------------------------------------------------
 
         # if 'specified_source=True'
         # this is used after clustering
-        df_trk1['numQueuedPerYear']=df_trk1['numQueuedPerYear'].replace(0,-1)
-        df_trk1['minQueuedPerYear']=df_trk1['minQueuedPerYear'].replace(0,-1)
-        df_trk1['RatioOfCompletion_num']=np.abs(df_trk1['numCompletedFromQueue']/df_trk1['numQueuedPerYear'])
-        df_trk1['RatioOfCompletion_min']=np.abs(df_trk1['minCompletedFromQueue']/df_trk1['minQueuedPerYear'])
+        df_trk1['numQueued']=df_trk1['numQueued'].replace(0,-1)
+        df_trk1['minQueued']=df_trk1['minQueued'].replace(0,-1)
+        df_trk1['RatioOfCompletion_num']=np.abs(df_trk1['numCompletedFromQueue']/df_trk1['numQueued'])
+        df_trk1['RatioOfCompletion_min']=np.abs(df_trk1['minCompletedFromQueue']/df_trk1['minQueued'])
         #replace -1 back to 0
-        df_trk1['numQueuedPerYear']=df_trk1['numQueuedPerYear'].replace(-1,0)
-        df_trk1['minQueuedPerYear']=df_trk1['minQueuedPerYear'].replace(-1,0)
+        df_trk1['numQueued']=df_trk1['numQueued'].replace(-1,0)
+        df_trk1['minQueued']=df_trk1['minQueued'].replace(-1,0)
         # drop min and 'isCompleted'
         df_trk1=df_trk1.drop(['min','isCompleted'],axis=1)
 
